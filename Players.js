f317a06618855d0f515e6b77358d27cdf5a0c570
ShantottoCast <shantottocast@gmail.com>
2024-12-10 16:06:18 -0600

    First Commit

---
+++ Players.js
@@ -0,0 +1,5 @@
+//check list of players
+//keep list of all enrolled players
+//make list of all recently enrolled players in "welcome to the league" list
+//check how many times a name shows up, rank them in list by count
+//
